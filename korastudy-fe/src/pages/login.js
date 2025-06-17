1018e0f2aa2f6b64ac26727fc571cf7703282509
trunghieu0716 <nguyentrung712003@gmail.com>
2025-06-17 09:22:19 +0700

    Update login.js

--- korastudy-fe/src/pages/login.js
+++ korastudy-fe/src/pages/login.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Eye, EyeOff } from 'lucide-react';
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
+import NavBar from '../../components/NavBar';
+import Footer from '../../components/Footer';
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
